efe748d469ca903bf188152f27bb735fcc3838d5
developer-mahin <mdmahin1310@gmail.com>
2024-05-01 10:54:04 +0600

    complete register and login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,105 @@
+import assets from "@/assets";
+import {
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+
+type TLoginData = {
+  name: string;
+  label: string;
+  type: string;
+};
+
+const loginData: TLoginData[] = [
+  {
+    name: "email",
+    label: "Email",
+    type: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+  },
+];
+
 const LoginPage = () => {
   return (
-    <div>
-      <h2>Welcome to the Page page</h2>
-    </div>
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        height: "100vh",
+      }}
+    >
+      <Container
+        maxWidth="sm"
+        sx={{
+          padding: "50px",
+          boxShadow: 1,
+          borderRadius: "10px",
+        }}
+      >
+        <Stack alignItems="center">
+          <Image src={assets.svgs.logo} alt="" width={50} height={50} />
+          <Typography variant="h4" component="h4" mt={1} fontWeight={600}>
+            Login PH HealthCare
+          </Typography>
+        </Stack>
+        <form className="mt-6">
+          <Grid container spacing={3}>
+            {loginData.map((data: TLoginData, i: number) => (
+              <Grid item key={i} md={6}>
+                <TextField
+                  label={data.label}
+                  type={data.type}
+                  name={data.name}
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                />
+              </Grid>
+            ))}
+          </Grid>
+
+          <Typography
+            textAlign="end"
+            sx={{
+              mt: 1,
+            }}
+          >
+            <Link href="/register">Forgot Password?</Link>
+          </Typography>
+
+          <Button
+            type="submit"
+            fullWidth
+            sx={{
+              mt: 3,
+            }}
+          >
+            Login
+          </Button>
+          <Typography
+            sx={{
+              textAlign: "center",
+              mt: 1,
+            }}
+          >
+            Don&apost have an account?{" "}
+            <Link href="/register" className="text-[#1586FD] font-medium">
+              Create an account
+            </Link>
+          </Typography>
+        </form>
+      </Container>
+    </Stack>
   );
 };
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,117 @@
+import {
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
+import Link from "next/link";
+
+type TRegister = {
+  column: number;
+  name: string;
+  label: string;
+  type: string;
+};
+
+const registerData: TRegister[] = [
+  {
+    column: 12,
+    name: "name",
+    label: "Name",
+    type: "text",
+  },
+  {
+    column: 6,
+    name: "email",
+    label: "Email",
+    type: "email",
+  },
+  {
+    column: 6,
+    name: "password",
+    label: "Password",
+    type: "password",
+  },
+  {
+    column: 6,
+    name: "contactNumber",
+    label: "Contact Number",
+    type: "text",
+  },
+  {
+    column: 6,
+    name: "address",
+    label: "Address",
+    type: "text",
+  },
+];
+
 const RegisterPages = () => {
   return (
-    <div>
-      <h2>Welcome to the Page page</h2>
-    </div>
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        height: "100vh",
+      }}
+    >
+      <Container
+        maxWidth="sm"
+        sx={{
+          padding: "50px",
+          boxShadow: 1,
+          borderRadius: "10px",
+        }}
+      >
+        <Stack alignItems="center">
+          <Image src={assets.svgs.logo} alt="" width={50} height={50} />
+          <Typography variant="h4" component="h4" mt={1} fontWeight={600}>
+            Patient Register
+          </Typography>
+        </Stack>
+        <form className="mt-6">
+          <Grid container spacing={3}>
+            {registerData.map((data: TRegister, i: number) => (
+              <Grid item key={i} md={data.column}>
+                <TextField
+                  label={data.label}
+                  type={data.type}
+                  name={data.name}
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                />
+              </Grid>
+            ))}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            sx={{
+              mt: 3,
+            }}
+          >
+            Register
+          </Button>
+          <Typography
+            sx={{
+              textAlign: "center",
+              mt:1,
+            }}
+
+          >
+            Do you already have an account?{" "}
+            <Link href="/login" className="text-[#1586FD] font-medium">
+              Login
+            </Link>
+          </Typography>
+        </form>
+      </Container>
+    </Stack>
   );
 };
 
