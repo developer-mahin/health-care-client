bc97e0d2b0228dba49066afb8941b2d631b7d042
developer-mahin <mdmahin1310@gmail.com>
2024-05-07 00:22:45 +0600

    created axios instace

---
+++ src/helper/axios/axiosInstance.ts
@@ -0,0 +1,50 @@
+import { authKey } from "@/constant/authKey";
+import { TGenericErrorResponse, TResponseSuccessType } from "@/types";
+import { getFromLocalStorage } from "@/utils/local-storage";
+import axios from "axios";
+
+const instance = axios.create();
+
+instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.headers["Accept"] = "application/json";
+instance.defaults.timeout = 60000;
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = getFromLocalStorage(authKey);
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  //@ts-ignore
+  function (response) {
+    const responseObject: TResponseSuccessType = {
+      data: response?.data?.data,
+      meta: response?.data?.meta,
+    };
+
+    return responseObject;
+  },
+  function (error) {
+    const responseObject: TGenericErrorResponse = {
+      statusCode: error?.response?.data?.statusCode || 500,
+      message: error?.response?.data?.message || "Something went wrong!!!",
+      errorMessages: error?.response?.data?.message,
+    };
+
+    return responseObject;
+  }
+);
+
+export { instance };
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,4 +1,4 @@
-import { axiosBaseQuery } from "@/helper/axiosBaseQuery";
+import { axiosBaseQuery } from "@/helper/axios/axiosBaseQuery";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
